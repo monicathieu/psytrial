a842e59a3a98c071c0e3f60171a06fc3b342b9b2
monicathieu <mkt2131@columbia.edu>
2018-02-20 17:49:48 -0500

    improving implementation for one.sim.psyphys2afc

--- R/psychophysics-2afc.R
+++ R/psychophysics-2afc.R
@@ -1,5 +1,8 @@
 #' Generate fake data for one subject, 2AFC psychophysics-style task
+#' 
+#' @importFrom magrittr %>%
 #'
+#' @param trash A throwaway first argument to allow piping from apply functions like replicate.
 #' @param bins A vector of numeric values, indicating the levels
 #' of intensity at which stimuli will be presented.
 #' @param mean A numeric value specifying the "true mean" from which
@@ -11,42 +14,40 @@
 #' @return The estimated mean and standard deviation of the probit model
 #' fit to one round of simulated data.
 
-one.psychometric.sim <- function (bins,
+one.sim.psyphys2afc <- function (trash = NULL,
+                                  xmin = NULL,
+                                  xmax = NULL,
+                                  xstep = NULL,
+                                  n.xlevels = NULL,
                                   mean,
                                   sd,
                                   trials.per.bin) {
-  bin.probs = pnorm(bins, mean = mean, sd = sd)
-  sim.data = as.data.frame(t(replicate(1, rbinom(n = length(bins),
+  
+  if (!is.null(xstep) & !is.null(n.xlevels)) warning("Both step size and # levels specified! Defaulting to use # levels.")
+  if (!is.null(n.xlevels)) {
+    xlevels = seq(xmin, xmax, length.out = n.xlevels)
+  } else if (!is.null(xstep)) {
+    xlevels = seq(xmin, xmax, xstep)
+  }
+  
+  
+  bin.probs = pnorm(xlevels, mean = mean, sd = sd)
+  sim.data = as.data.frame(t(replicate(1, rbinom(n = length(xlevels),
                                                  size = trials.per.bin,
                                                  prob = bin.probs)))) %>%
     mutate(fake.subj.num = 1:n()) %>%
-    gather(key = "bin", value = "n.yes.resps", -fake.subj.num) %>%
-    mutate(bin = as.numeric(plyr::mapvalues(bin, from = unique(bin), to = bins)),
+    gather(key = "xlevel", value = "n.yes.resps", -fake.subj.num) %>%
+    mutate(xlevel = as.numeric(plyr::mapvalues(xlevel, from = unique(xlevel), to = xlevels)),
            n.no.resps = trials.per.bin - n.yes.resps,
            prop.resps = n.yes.resps / trials.per.bin)
-
-  sim.model = summary(glm(cbind(n.yes.resps, n.no.resps) ~ bin,
+  
+  sim.model = summary(glm(cbind(n.yes.resps, n.no.resps) ~ xlevel,
                           family = binomial(link = "probit"),
                           data = sim.data))
-
+  
   # calculating the mean and standard deviation of cumulative normal from probit params
   sim.model.params = unname(c(-(sim.model$coefficients[1, "Estimate"] / sim.model$coefficients[2, "Estimate"]),
                               1/sim.model$coefficients[2, "Estimate"]))
-
-return (sim.model.params)
-}
-
-multi.binsize.params <- function (binsizes = c(4, 5, 10, 20, 40),
-                                  n.bins = 5,
-                                  n.reps = 100,
-                                  mean = .561,
-                                  sd = .212) {
-  params = lapply(as.list(binsizes),
-                  function(x) get.rep.params(n.reps = n.reps,
-                                             bins = seq(0, 0.8, 0.8 / (n.bins - 1)),
-                                             mean = mean,
-                                             sd = sd,
-                                             trials.per.bin = x))
-  names(params) = binsizes
-  return (params)
+  
+  return (sim.model.params)
 }
