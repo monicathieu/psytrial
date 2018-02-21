bcc1511db4f5dc21d79adcc0e674962117f9ce81
monicathieu <mthieu@stanford.edu>
2018-02-20 21:01:14 -0500

    justifying?

--- R/psychophysics-2afc.R
+++ R/psychophysics-2afc.R
@@ -1,7 +1,7 @@
 #' Generate fake data for one subject, 2AFC psychophysics-style task
-#' 
+#'
 #' Fits a probit/cumulative normal model to fake single-subject data.
-#' 
+#'
 #' @importFrom dplyr mutate
 #' @importFrom magrittr %>%
 #' @importFrom plyr mapvalues
@@ -36,15 +36,14 @@ one.sim.psyphys2afc <- function (trash = NULL,
                                  mean,
                                  sd,
                                  trials.per.bin) {
-  
+
   if (!is.null(xstep) & !is.null(n.xlevels)) warning("Both step size and # levels specified! Defaulting to use # levels.")
   if (!is.null(n.xlevels)) {
     xlevels = seq(xmin, xmax, length.out = n.xlevels)
   } else if (!is.null(xstep)) {
     xlevels = seq(xmin, xmax, xstep)
   }
-  
-  
+
   bin.probs = pnorm(xlevels, mean = mean, sd = sd)
   sim.data = as.data.frame(t(replicate(1, rbinom(n = length(xlevels),
                                                  size = trials.per.bin,
@@ -54,14 +53,14 @@ one.sim.psyphys2afc <- function (trash = NULL,
     dplyr::mutate(xlevel = as.numeric(plyr::mapvalues(xlevel, from = unique(xlevel), to = xlevels)),
                   n.no.resps = trials.per.bin - n.yes.resps,
                   prop.resps = n.yes.resps / trials.per.bin)
-  
+
   sim.model = summary(stats::glm(cbind(n.yes.resps, n.no.resps) ~ xlevel,
                                  family = binomial(link = "probit"),
                                  data = sim.data))
-  
+
   # calculating the mean and standard deviation of cumulative normal from probit params
   sim.model.params = unname(c(-(sim.model$coefficients[1, "Estimate"] / sim.model$coefficients[2, "Estimate"]),
                               1/sim.model$coefficients[2, "Estimate"]))
-  
+
   return (sim.model.params)
 }
