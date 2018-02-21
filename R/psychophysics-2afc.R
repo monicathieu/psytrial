65ad3341922114453d6ccd46a334591d1665bea7
monicathieu <mthieu@stanford.edu>
2018-02-21 00:19:40 -0500

    cleaning up <- operators & tests

--- R/psychophysics-2afc.R
+++ R/psychophysics-2afc.R
@@ -39,27 +39,27 @@ one.sim.psyphys2afc <- function (trash = NULL,
 
   if (!is.null(xstep) & !is.null(n.xlevels)) warning("Both step size and # levels specified! Defaulting to use # levels.")
   if (!is.null(n.xlevels)) {
-    xlevels = seq(xmin, xmax, length.out = n.xlevels)
+    xlevels <- seq(xmin, xmax, length.out = n.xlevels)
   } else if (!is.null(xstep)) {
-    xlevels = seq(xmin, xmax, xstep)
+    xlevels <- seq(xmin, xmax, xstep)
   }
 
-  bin.probs = pnorm(xlevels, mean = mean, sd = sd)
-  sim.data = as.data.frame(t(replicate(1, rbinom(n = length(xlevels),
+  bin.probs <- pnorm(xlevels, mean = mean, sd = sd)
+  sim.data <- as.data.frame(t(rbinom(n = length(xlevels),
                                                  size = trials.per.bin,
-                                                 prob = bin.probs)))) %>%
+                                                 prob = bin.probs))) %>%
     dplyr::mutate(fake.subj.num = 1:n()) %>%
     tidyr::gather(key = "xlevel", value = "n.yes.resps", -fake.subj.num) %>%
     dplyr::mutate(xlevel = as.numeric(plyr::mapvalues(xlevel, from = unique(xlevel), to = xlevels)),
                   n.no.resps = trials.per.bin - n.yes.resps,
                   prop.resps = n.yes.resps / trials.per.bin)
 
-  sim.model = summary(stats::glm(cbind(n.yes.resps, n.no.resps) ~ xlevel,
+  sim.model <- summary(stats::glm(cbind(n.yes.resps, n.no.resps) ~ xlevel,
                                  family = binomial(link = "probit"),
                                  data = sim.data))
 
   # calculating the mean and standard deviation of cumulative normal from probit params
-  sim.model.params = unname(c(-(sim.model$coefficients[1, "Estimate"] / sim.model$coefficients[2, "Estimate"]),
+  sim.model.params <- unname(c(-(sim.model$coefficients[1, "Estimate"] / sim.model$coefficients[2, "Estimate"]),
                               1/sim.model$coefficients[2, "Estimate"]))
 
   return (sim.model.params)
--- tests/testthat/test-psychophysics-2afc.R
+++ tests/testthat/test-psychophysics-2afc.R
@@ -1,7 +1,43 @@
-context("Single subject simulation for 2AFC psychophysics-style expt")
+context("Simulations for 2AFC psychophysics-style expt")
 
-test_that("Outputs parameter estimates properly", {
+test_that("Single sim outputs parameter estimates properly", {
   expect_length(one.sim.psyphys2afc(xmin = 0, xmax = 0.8,
                                     n.xlevels = 5, mean = 0.5, sd = 0.2,
                                     trials.per.bin = 20), 2)
+})
+
+test_that("Repeated sim outputs parameter estimates properly", {
+  expect_equal(nrow(replicate.sim(type = "psychophysics.2afc",
+                                  n.reps = 5,
+                                  xmin = 0, xmax = 0.8,
+                                  n.xlevels = 5, mean = 0.5, sd = 0.2,
+                                  trials.per.bin = 20)), 5)
+  expect_equal(ncol(replicate.sim(type = "psychophysics.2afc",
+                                  n.reps = 5,
+                                  xmin = 0, xmax = 0.8,
+                                  n.xlevels = 5, mean = 0.5, sd = 0.2,
+                                  trials.per.bin = 20)), 2)
+})
+
+test_that("Repeated sim by different expt parameter levels outputs parameter estimates properly", {
+  expect_equal(nrow(replicate.sim.by.param(type = "psychophysics.2afc",
+                                           n.reps = 5,
+                                           xmin = 0, xmax = 0.8,
+                                           n.xlevels = c(3,5), mean = 0.5, sd = 0.2,
+                                           trials.per.bin = 20)), 10)
+  expect_equal(ncol(replicate.sim.by.param(type = "psychophysics.2afc",
+                                           n.reps = 5,
+                                           xmin = 0, xmax = 0.8,
+                                           n.xlevels = c(3,5), mean = 0.5, sd = 0.2,
+                                           trials.per.bin = 20)), 3)
+  expect_equal(nrow(replicate.sim.by.param(type = "psychophysics.2afc",
+                                           n.reps = 5,
+                                           xmin = 0, xmax = 0.8,
+                                           n.xlevels = c(3,5), mean = 0.5, sd = 0.2,
+                                           trials.per.bin = c(10, 20))), 20)
+  expect_equal(ncol(replicate.sim.by.param(type = "psychophysics.2afc",
+                                           n.reps = 5,
+                                           xmin = 0, xmax = 0.8,
+                                           n.xlevels = c(3,5), mean = 0.5, sd = 0.2,
+                                           trials.per.bin = c(10, 20))), 4)
 })
